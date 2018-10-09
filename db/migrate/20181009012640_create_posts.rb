ec8632d047238ded6e607c22458576dacc309b4b
Sarah Kirk <kirkse710@gmail.com>
2018-10-08 18:27:22 -0700

    adds posts model with user references

---
+++ db/migrate/20181009012640_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :body
+      t.string :image
+      t.string :user
+      t.string :references
+
+      t.timestamps
+    end
+  end
+end
