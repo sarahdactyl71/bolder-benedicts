7378704c60c108092d3a808e31774815d11206c0
Sarah Kirk <kirkse710@gmail.com>
2018-10-08 19:28:48 -0700

    untracks secrets.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+    sarah = User.new(email: 'kirkse710@gmail.com')
+    sarah.encrypted_password = '$2a$11$C/Uy0lj/iQ3EM67nIEeWQejwZq1lQ.HIejjJE.bK1t8tenCGZa34e'
+    sarah.save!
